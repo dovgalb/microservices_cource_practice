81336dc5c7b0cd83f57d727f8e98873597ef413a
dovgalb <bogdandovgal1208@gmail.com>
2025-07-07 15:58:25 +0300

    test gh

--- week_1/grpc/go.mod
+++ week_1/grpc/go.mod
@@ -2,10 +2,18 @@ module github.com/dovgalb/microservices_cource_practice/week_1/grpc
 
 go 1.23.6
 
-require github.com/fatih/color v1.18.0
+require (
+	github.com/brianvoe/gofakeit v3.18.0+incompatible
+	github.com/fatih/color v1.18.0
+	google.golang.org/grpc v1.73.0
+	google.golang.org/protobuf v1.36.6
+)
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
 )
---
+++ week_2/grpc/cmd/grpc_client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	desc "github.com/dovgalb/microservices_cource_practice/week_2/grpc/pkg/note_v1"
+	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"time"
+)
+
+const (
+	address = "localhost:50051"
+	noteID  = 12
+)
+
+func main() {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to server: %v", err)
+	}
+
+	defer conn.Close()
+
+	c := desc.NewNoteV1Client(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Get(ctx, &desc.GetRequest{Id: noteID})
+	if err != nil {
+		log.Fatalf("Failed to get noty by id: %v", err)
+	}
+
+	log.Printf(color.RedString("Note info:\n"), color.GreenString("%+v", r.GetNote()))
+
+}
